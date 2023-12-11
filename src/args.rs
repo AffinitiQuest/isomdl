de477823d8aac4c6c593b855f857e70ba70e0053
Warren Gallagher <warren@affinitiquest.io>
2023-12-11 17:22:37 -0500

    Minor changes to mdl.json. New command-line parser. Integrate new command-line parser.

---
+++ src/args.rs
@@ -0,0 +1,37 @@
+use clap:: {
+    Args,
+    Parser,
+    Subcommand
+};
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+pub struct IsoMdlArgs {
+    /// subcommand is either "issue" or "verify"
+    #[clap(subcommand)]
+    pub subcommand: IsoMdlCommand,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum IsoMdlCommand {
+    /// Issue an mdoc based mDL based on a provided input json file
+    Issue(IssueCommand),
+    /// Verify an mdoc based mDL based on a provided input mdoc file
+    Verify(VerifyCommand)
+}
+
+#[derive(Debug, Args)]
+pub struct IssueCommand {
+    /// input is a json file that supplies the claims values to be in the issued mdoc-based mDL
+    pub input_filename: String,
+    /// optional output file is issued mdoc , if not specified, then standard out is used
+    pub output_filename: Option<String>
+}
+
+#[derive(Debug, Args)]
+pub struct VerifyCommand {
+    /// input is a mdoc-based mDL file to be verified
+    pub input_filename: String,
+    /// optional output file is parsed claims from verified mdoc, if not specified, then standard out is used
+    pub output_filename: Option<String>
+}
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,197 @@
-use std::collections::BTreeMap;
 
 use anyhow::{Context, Error};
 use clap::Parser;
-use clap_stdin::MaybeStdin;
 use isomdl::presentation::{device::Document, Stringify};
-use serde_cbor::Value;
+use serde_cbor::Value as CborValue;
+mod args;
+use args::{IsoMdlArgs, IsoMdlCommand, IssueCommand, VerifyCommand};
+use serde_json::Value as JsonValue;
+use std::{
+    path::Path,
+    fs::File,
+    io::{BufWriter, Write}
+};
 
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    #[command(subcommand)]
-    action: Action,
-}
-
-#[derive(Debug, clap::Subcommand)]
-enum Action {
-    /// Print the namespaces and element identifiers used in an mDL.
-    GetNamespaces {
-        /// Base64 encoded mDL in the format used in the issuance module of this crate.
-        mdl: MaybeStdin<String>,
-    },
-    /// Print the namespaces and element identifiers used in an mDL.
-    GetClaims {
-        /// Base64 encoded mDL in the format used in the issuance module of this crate.
-        mdl: MaybeStdin<String>,
-    },
-    // create a new mdoc based on a JSON string passed in on the command line
-    // CreateMdoc {
-    //     /// Base64 encoded mDL in the format used in the issuance module of this crate.
-    //     json: MaybeStdin<String>,
-    // },
-}
+use isomdl::definitions::traits::{FromJson, ToNamespaceMap};
+use isomdl::definitions::namespaces::org_iso_18013_5_1::OrgIso1801351;
 
 fn main() -> Result<(), Error> {
-    match Args::parse().action {
-        Action::GetNamespaces { mdl } => {
-            print_namespaces(mdl.to_string())
+    let args : IsoMdlArgs = IsoMdlArgs::parse();
+    println!("{:?}", args);
+
+    match args.subcommand {
+        IsoMdlCommand::Issue(issue_command) => {
+            issue_mdl(issue_command)
         },
-        Action::GetClaims { mdl } => {
-            print_claims(mdl.to_string())
-        },
-        // Action::CreateMdoc { json } => {
-        //     create_mdoc_from_json_string( json.to_string())
-        // },
+        IsoMdlCommand::Verify(verify_command) => {
+            verify_mdl(verify_command)
+        }
     }
 }
 
-fn print_namespaces(mdl: String) -> Result<(), Error> {
-    let claims = Document::parse(mdl)
-        .context("could not parse mdl")?
-        .namespaces
-        .into_inner()
-        .into_iter()
-        .map(|(ns, inner)| (ns, inner.into_inner().into_keys().collect()))
-        .collect::<BTreeMap<String, Vec<String>>>();
-    println!("{}", serde_json::to_string_pretty(&claims)?);
+fn issue_mdl( issue_command: IssueCommand ) -> Result<(), Error> {
+    let content = std::fs::read_to_string(&issue_command.input_filename)
+        .context(format!("could not read input_filename {}", &issue_command.input_filename))?;
+
+    let v: JsonValue = serde_json::from_str(&content)?;
+    // iterate over the namespaces
+    let namespaces: &JsonValue = &v["namespace"];
+    match namespaces {
+        JsonValue::Object(o) => {
+            for namespace in o.keys() {
+                let value = o.get_key_value(namespace);
+                println!("namespace={}", namespace);
+                let doc_type = String::from("org.iso.18013.5.1.mDL");
+                
+                let mdl_data = OrgIso1801351::from_json (value)         
+                    .unwrap()
+                    .to_ns_map();            
+            }
+        },
+        __hidden => {}
+    }
+
     Ok(())
+    // let out_writer = match issue_command.output_filename {
+    //     Some(x) => {
+    //         let path = Path::new(&x);
+    //         Box::new(File::create(&path).unwrap()) as Box<dyn Write>
+    //     }
+    //     None => Box::new(std::io::stdout()) as Box<dyn Write>,
+    // };
+
+    // let mut buf = BufWriter::new(out_writer);
+    // writeln!(buf, "{:?}", &content)
+    //     .context("Error writing to output file")
+}
+
+// fn minimal_mdoc_builder() -> Builder {
+//     let doc_type = String::from("org.iso.18013.5.1.mDL");
+//     let isomdl_namespace = String::from("org.iso.18013.5.1");
+//     let aamva_namespace = String::from("org.iso.18013.5.1.aamva");
+
+//     let isomdl_data = OrgIso1801351::from_json(&isomdl_data())
+//         .unwrap()
+//         .to_ns_map();
+//     let aamva_data = OrgIso1801351Aamva::from_json(&aamva_isomdl_data())
+//         .unwrap()
+//         .to_ns_map();
+
+//     let namespaces = [
+//         (isomdl_namespace, isomdl_data),
+//         (aamva_namespace, aamva_data),
+//     ]
+//     .into_iter()
+//     .collect();
+
+//     let validity_info = ValidityInfo {
+//         signed: OffsetDateTime::now_utc(),
+//         valid_from: OffsetDateTime::now_utc(),
+//         valid_until: OffsetDateTime::now_utc(),
+//         expected_update: None,
+//     };
+
+//     let digest_algorithm = DigestAlgorithm::SHA256;
+
+//     let der = include_str!("../../test/issuance/device_key.b64");
+//     let der_bytes = base64::decode(der).unwrap();
+//     let key = p256::SecretKey::from_sec1_der(&der_bytes).unwrap();
+//     let pub_key = key.public_key();
+//     let ec = pub_key.to_encoded_point(false);
+//     let x = ec.x().unwrap().to_vec();
+//     let y = EC2Y::Value(ec.y().unwrap().to_vec());
+//     let device_key = CoseKey::EC2 {
+//         crv: EC2Curve::P256,
+//         x,
+//         y,
+//     };
+
+//     let device_key_info = DeviceKeyInfo {
+//         device_key,
+//         key_authorizations: None,
+//         key_info: None,
+//     };
+
+//     Mdoc::builder()
+//         .doc_type(doc_type)
+//         .namespaces(namespaces)
+//         .validity_info(validity_info)
+//         .digest_algorithm(digest_algorithm)
+//         .device_key_info(device_key_info)
+// }
+
+
+fn verify_mdl( verify_command: VerifyCommand ) -> Result<(), Error> {
+    let content = std::fs::read_to_string(&verify_command.input_filename)
+        .context(format!("could not read input_filename {}", &verify_command.input_filename))?;
+
+    let out_writer = match verify_command.output_filename {
+        Some(x) => {
+            let path = Path::new(&x);
+            Box::new(File::create(&path).unwrap()) as Box<dyn Write>
+        }
+        None => Box::new(std::io::stdout()) as Box<dyn Write>,
+    };
+    let buf = BufWriter::new(out_writer);
+    print_claims(content, buf)
+}
+
+fn print_claims(mdl: String, mut output_buffer: BufWriter<Box<dyn Write>>) -> Result<(), Error> {
+
+    let parsed = Document::parse(mdl).context("could not parse mdl");
+    let namespaces = parsed?.namespaces;
+    for namespace in namespaces.into_inner() {
+        let namespace_name = namespace.0;
+        writeln!(output_buffer, "namespace={:#?}", namespace_name)
+            .context("Error writing to output file")?;
+        for key in namespace.1.into_inner() {
+            let key_name = key.0;
+            let value = key.1.into_inner().element_value;
+            
+            // println!("{}", serde_json::to_string_pretty(&key.1.into_inner())?);
+            let string_value = convert_value_to_string(value);
+            if key_name != "portrait" {
+                writeln!(output_buffer, "  '{}': {}", key_name, string_value)
+                    .context("Error writing to output file")?;
+            }
+        }
+    }
+    
+    Ok(())
+}
+fn convert_value_to_string(value: CborValue) -> String {
+    let string_value;
+    match value {
+        CborValue::Null => string_value=String::from(""),
+        CborValue::Text (v) => string_value=format!("'{}'", serde_cbor::value::from_value::<String>(serde_cbor::Value::Text(v)).unwrap()),
+        CborValue::Bool (v) => string_value=format!("bool={}", serde_cbor::value::from_value::<bool>(serde_cbor::Value::Bool(v)).unwrap().to_string()),
+        CborValue::Integer (v) => string_value=format!("int={}", serde_cbor::value::from_value::<u128>(serde_cbor::Value::Integer(v)).unwrap().to_string()),
+        CborValue::Float (v) => string_value=format!("int={}", serde_cbor::value::from_value::<f64>(serde_cbor::Value::Float(v)).unwrap().to_string()),
+        CborValue::Bytes (_v) => string_value=String::from("bytes"),
+        CborValue::Array (_v) => {
+            let mut s = String::from("[ ").to_owned();
+            for val in _v {
+                s.push_str(&convert_value_to_string(val));
+                s.push_str(", ");
+            }
+            s.push_str("]");
+            string_value = s;
+        },
+        CborValue::Map (_v) => {
+            let mut s: String = String::from("{ ");
+            for (key, val) in _v {
+                s.push_str(&convert_value_to_string(key));
+                s.push_str(": ");
+                s.push_str(&convert_value_to_string(val));
+            }
+            s.push_str("}");
+            string_value = s;
+        },
+        //string_value=String::from("{}"),//string_value=String::from(format!("{}", convert_value_to_string(serde_cbor::Value::Map(_v)))),
+        CborValue::Tag (_a, _v) => string_value=format!("tag=({}, {})", convert_tag_to_string(_a), convert_value_to_string(*_v)),
+        CborValue::__Hidden => string_value=String::from(""),
+    }
+    return string_value;
 }
 
 fn convert_tag_to_string(value: u64) -> String {
@@ -68,67 +204,4 @@ fn convert_tag_to_string(value: u64) -> String {
     return format!("{}", value);
 }
 
-fn convert_value_to_string(value: serde_cbor::value::Value) -> String {
-    let string_value;
-    match value {
-        Value::Null => string_value=String::from(""),
-        Value::Text (v) => string_value=format!("'{}'", serde_cbor::value::from_value::<String>(serde_cbor::Value::Text(v)).unwrap()),
-        Value::Bool (v) => string_value=format!("bool={}", serde_cbor::value::from_value::<bool>(serde_cbor::Value::Bool(v)).unwrap().to_string()),
-        Value::Integer (v) => string_value=format!("int={}", serde_cbor::value::from_value::<u128>(serde_cbor::Value::Integer(v)).unwrap().to_string()),
-        Value::Float (v) => string_value=format!("int={}", serde_cbor::value::from_value::<f64>(serde_cbor::Value::Float(v)).unwrap().to_string()),
-        Value::Bytes (_v) => string_value=String::from("bytes"),
-        Value::Array (_v) => {
-            let mut s = String::from("[ ").to_owned();
-            for val in _v {
-                s.push_str(&convert_value_to_string(val));
-                s.push_str(", ");
-            }
-            s.push_str("]");
-            string_value = s;
-        },
-        Value::Map (_v) => {
-            let mut s: String = String::from("{ ");
-            for (key, val) in _v {
-                s.push_str(&convert_value_to_string(key));
-                s.push_str(": ");
-                s.push_str(&convert_value_to_string(val));
-            }
-            s.push_str("}");
-            string_value = s;
-        },
-        //string_value=String::from("{}"),//string_value=String::from(format!("{}", convert_value_to_string(serde_cbor::Value::Map(_v)))),
-        Value::Tag (_a, _v) => string_value=format!("tag=({}, {})", convert_tag_to_string(_a), convert_value_to_string(*_v)),
-        Value::__Hidden => string_value=String::from(""),
-    }
-    return string_value;
-}
 
-fn print_claims(mdl: String) -> Result<(), Error> {
-
-    let parsed = Document::parse(mdl).context("could not parse mdl");
-    let namespaces = parsed?.namespaces;
-    for namespace in namespaces.into_inner() {
-        let namespace_name = namespace.0;
-        println!("namespace={:#?}", namespace_name);
-        for key in namespace.1.into_inner() {
-            let key_name = key.0;
-            let value = key.1.into_inner().element_value;
-            
-            // println!("{}", serde_json::to_string_pretty(&key.1.into_inner())?);
-            let string_value = convert_value_to_string(value);
-            if key_name != "portrait" {
-                println!("  '{}': {}", key_name, string_value);
-            }
-        }
-    }
-    
-    Ok(())
-}
-
-#[cfg(test)]
-mod test {
-    #[test]
-    fn print_namespaces() {
-        super::print_namespaces(include_str!("../test/stringified-mdl.txt").to_string()).unwrap()
-    }
-}
